b9b8638b7edcd22b9ef0ad895020f7ab4088a8b2
nathan <nathan@dorkbox.com>
2020-08-07 15:23:48 +0200

    Updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 dorkbox, llc
+ * Copyright 2020 dorkbox, llc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ import java.time.Instant
 
 gradle.startParameter.showStacktrace = ShowStacktrace.ALWAYS   // always show the stacktrace!
 gradle.startParameter.warningMode = WarningMode.All
-
 
 plugins {
     java
