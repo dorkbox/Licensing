6a82051eb5261acdc6f8bccb04f61c273b8c0b1b
Robinson <robinson@dorkbox.com>
2021-09-22 02:59:37 +0200

    Updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,9 @@ plugins {
 
     id("com.gradle.plugin-publish") version "0.14.0"
 
-    id("com.dorkbox.Licensing") version "2.9"
-    id("com.dorkbox.VersionUpdate") version "2.3"
-    id("com.dorkbox.GradleUtils") version "2.8.1"
+    id("com.dorkbox.Licensing") version "2.9.2"
+    id("com.dorkbox.VersionUpdate") version "2.4"
+    id("com.dorkbox.GradleUtils") version "2.13"
 
     kotlin("jvm") version "1.5.21"
 }
@@ -36,7 +36,7 @@ object Extras {
     // set for the project
     const val description = "License definitions and legal management plugin for the Gradle build system"
     const val group = "com.dorkbox"
-    const val version = "2.9.2"
+    const val version = "3.0"
 
     // set as project.ext
     const val name = "Gradle Licensing Plugin"
